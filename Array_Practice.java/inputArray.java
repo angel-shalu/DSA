904bca46515370d404cb15033e45d06d1806aaee
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2025-07-03 20:04:32 +0530

    added

--- Array_Practice.java/inputArray.java
+++ Array_Practice.java/inputArray.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class inputArray {
+    public static void main(String[] args) {
+        int n = 5;
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Dispaly ;");
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
     
 }
