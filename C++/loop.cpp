63672e4f5881883672e1eb5d9f1cbfd42c98a7ca
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2025-09-02 12:49:39 +0530

    done

--- C++/loop.cpp
+++ C++/loop.cpp
@@ -49,34 +49,89 @@ int main()
     //     cout << i << endl;
     // }
 
+    // ==========
     // int num;
     // cout << "Enter the number: ";
     // cin >> num;
 
-    // for (int i = 1; i <= num; i++) // increment by 2
+    // for (int i = 1; i <= num; i++) // increment by 1
     // {
     //     if (i % 2 == 0)
     //     {
-    //         cout << i << " ";
+    //         cout << i <<endl;
     //     }
     //     else
     //     {
     //     }
     // }
 
-    int num;
-    cout << "Enter the number: ";
-    cin >> num;
-    for (int i = 1; i <= num; i++) // increment by 2
-    {
-        if (i % 2 == 1)
-        {
-            cout << i << " ";
-        }
-        else
-        {
-        }
-    }
+    // // ============ 100 to 200=====
+    // int i;
+    // cout << "Enter the Number :";
+    // cin >> i;
+    // for (int i = 100; i <= 200; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // // ===== a to z====
+    // char name;
+    // // cout << "Enter the letter :";
+    // cin >> name;
+    // for (char name = 'a'; name <= 'z'; name++)
+    // {
+    //     cout << name << endl;
+    // }
+
+    // // ====Reverse number=====
+    // int num, i;
+    // cout << "Enter the NUmber:";
+    // cin >> num;
+    // for (i = num; i >= 1; i--)
+    // {
+    //     cout << i << endl; // print i, not num
+    // }
+
+    // // ==== Print num from 1 to 100 and step 3===
+    // int i;
+    // for (i = 1; i <= 100; i + 3)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // // ===== print 7 Table=====
+    // int n, i;
+    // cout << "Enter the Table :";
+    // cin >> n;
+    // for (i = 1; i <= 10; i++)
+    // {
+    //     // cout << n * i << endl;
+    //     cout << n << "*" << i << "=" << n * i << endl;
+    // }
+
+    // // =========
+    // int n, i;
+    // cout << "Enter the number :";
+    // cin >> n;
+    // for (int i = n; i <= 10 * n; i += n)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // //====Calculating the POWER  4*4*4*4 =====
+    // int n, pow, i, num;
+    // cout << "Enter the Number :";
+    // cin >> n;
+    // cout << "Enter the Power :";
+    // cin >> pow;
+    // num = n;
+    // for (int i = 1; i < pow; i++)
+    // {
+    //     num = num * n;
+    // }
+    // cout << num;
+
+    // ==== Sun of n natural number=====
 
     return 0;
 }
