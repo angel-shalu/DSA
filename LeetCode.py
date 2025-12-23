6a99c8bfdda4a73278d425404d2f35f5ca547e61
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2025-12-23 22:07:07 +0530

    done

--- LeetCode.py
+++ LeetCode.py
@@ -1,7 +1,8 @@
 # Q.1 Two Sum Problem in Python
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            sum = nums[i] + nums[j]
-            if sum == target:
-                return [i, j]            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return[i,j]
