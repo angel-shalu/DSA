5d8c7a7f5338eaac3aa34403df183d6028f49213
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2025-12-25 10:22:58 +0530

    done

--- LeetCode.py
+++ LeetCode.py
@@ -6,3 +6,64 @@ class Solution:
                 sum = nums[i] + nums[j]
                 if sum == target:
                     return[i,j]
+                
+                
+                
+# ----------------Q.14. Longest Common Prefix-------------------
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Initialize an empty result string
+        res = ""
+        
+        # Loop through each character index of the first string
+        for i in range(len(strs[0])):
+            ch = strs[0][i]  # Get the character at the current index
+            
+            # Compare this character with the corresponding character in each string
+            for s in strs:
+                # If we are out of bounds for any string, or there's a mismatch
+                if i >= len(s) or s[i] != ch:
+                    return res  # Return the common prefix found so far
+            
+            # If the character matches in all strings, add it to the result
+            res += ch
+        return res  # Return the longest common prefix
+
+
+        # ------------------
+        # cp=""
+        # for i in range(len(strs[0])):
+        #     ch = strs[0][i]
+        #     for s in strs:
+        #         if i == len(s) or s[i] != ch:
+        #             return cp
+        #             cp += ch
+        # return cp
+        # -------------------
+        
+        # pref = strs[0]
+        # pref_len = len(pref)
+        # for s in strs[1:]:
+        #     while pref != s[0:pref_len]:
+        #         pref_len -=1
+        #         if pref_len == 0:
+        #             return""
+        #             pref = pref[0:pref_len]
+                    
+        # return pref
+
+    
+        
+                        
+
+                        
+
+                        
+
+                        
+
+    
+
+
+
+        
