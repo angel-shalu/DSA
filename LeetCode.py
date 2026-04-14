b5cf4671e306f3bf9a06470e3763c1d32e997800
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2026-04-14 10:55:27 +0530

    Refactor multiple LeetCode solutions in LeetCode.py

--- LeetCode.py
+++ LeetCode.py
@@ -192,8 +192,17 @@ class Solution:
 
 
 # -------------Q. Ugly Number-----------------------------------
+class Solution:
+    def isUgly(self, n: int) -> bool:
 
-                        
+        if n <= 0:
+            return False
+
+        for i in [2, 3, 5]:
+            while n % i == 0:
+                n //= i
+
+        return n == 1
 
     
 
