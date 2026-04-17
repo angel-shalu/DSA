45cef9d96906133aef320331947206d6b817a331
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2026-04-17 21:36:03 +0530

    Update LeetCode.py

--- LeetCode.py
+++ LeetCode.py
@@ -219,6 +219,16 @@ class Solution:
 
 
 # ---------------Q. 342. Power of Four ----------------------
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+
+        if n <= 0:
+            return False
+
+        while n % 4 == 0:
+            n //= 4
+
+        return n == 1
 
     
 
